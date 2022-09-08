a889d9d4aac616dcd45d7b41434f97e2edc7b681
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-09-08 15:45:24 -0400

    payment bug fix for dev

--- src/pages/Dashboard/CheckoutForm.js
+++ src/pages/Dashboard/CheckoutForm.js
@@ -54,7 +54,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: 'http://localhost:3000/dashboard',
+        return_url: 'https://burgerqueenbyinam.netlify.app/dashboard',
       },
     })
 
